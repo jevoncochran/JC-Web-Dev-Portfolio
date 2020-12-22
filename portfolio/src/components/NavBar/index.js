a6acfdc9e200d01a0fbe67fe60cd68b5ea2cdace
Jevon Cochran <jevon.cochran@gmail.com>
2020-12-21 20:46:05 -0800

    corrected routes for gh pages

--- portfolio/src/components/NavBar/index.js
+++ portfolio/src/components/NavBar/index.js
@@ -5,22 +5,22 @@ import "./NavBar.scss";
 const NavBar = () => {
   return (
     <div className="nav-main">
-      <Link className="nav-link" to="/">
+      <Link className="nav-link" to="/JC-Web-Dev-Portfolio">
         <div className="nav-sub nav-headline-sub">
           <h1 className="nav-headline-text">JC</h1>
         </div>
       </Link>
       <div className="nav-sub nav-link-sub">
-        <Link className="nav-link" to="/about">
+        <Link className="nav-link" to="/JC-Web-Dev-Portfolio/about">
           About Me
         </Link>
-        <Link className="nav-link" to="/resume">
+        <Link className="nav-link" to="/JC-Web-Dev-Portfolio/resume">
           Resume
         </Link>
-        <Link className="nav-link" to="/projects">
+        <Link className="nav-link" to="/JC-Web-Dev-Portfolio/projects">
           Projects
         </Link>
-        <Link className="nav-link" to="/contact">
+        <Link className="nav-link" to="/JC-Web-Dev-Portfolio/contact">
           Contact
         </Link>
       </div>
